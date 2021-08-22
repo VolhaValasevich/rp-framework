9f1880246570c31de120734f5b9ca7c547e98b58
Volha_Valasevich <volha_valasevich@epam.com>
2021-08-22 15:18:09 +0300

    add option to filter specs by tag

--- e2e/config/jasmine.js
+++ e2e/config/jasmine.js
@@ -1,9 +1,11 @@
 const Jasmine = require('jasmine');
 const JasmineReporters = require("jasmine-reporters");
+const args = require('../utils/paramsHelper');
 
-const jasmine = new Jasmine();
+const jasmineRunner = new Jasmine();
+const tags = args.getTags();
 
-jasmine.loadConfig({
+jasmineRunner.loadConfig({
     spec_dir: 'e2e/specs',
     spec_files: [
         '*.spec.js'
@@ -13,19 +15,27 @@ jasmine.loadConfig({
     ]
 });
 
-jasmine.configureDefaultReporter({
+jasmineRunner.configureDefaultReporter({
     print: () => null
 });
 
-jasmine.addReporter(new JasmineReporters.JUnitXmlReporter({
+jasmineRunner.addReporter(new JasmineReporters.JUnitXmlReporter({
     consolidateAll: true,
     savePath: './reports/junit',
 }));
 
-jasmine.addReporter(new JasmineReporters.TerminalReporter({
+jasmineRunner.addReporter(new JasmineReporters.TerminalReporter({
     verbosity: 3,
     color: true,
     showStack: true
 }));
 
-jasmine.execute();
+jasmine.getEnv().specFilter = function (spec) {
+    const grepMatch = !tags || spec.getFullName().match(new RegExp(tags)) != null;
+    if (!grepMatch) {
+        spec.disable();
+    }
+    return true;
+}
+
+jasmineRunner.execute();
